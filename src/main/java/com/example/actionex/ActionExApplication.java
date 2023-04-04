e78f28fb6bfb1c9544350abd000da0f2686fefd1
Gonue <6017891@naver.com>
2023-04-04 12:31:53 +0900

    .

---
+++ src/main/java/com/example/actionex/ActionExApplication.java
@@ -0,0 +1,13 @@
+package com.example.actionex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActionExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/actionex/ActionExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actionex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
